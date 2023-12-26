8e2c53deec7a6f7726e675e12a14c9855343350a
oilprint <irynakozlova.k@gmail.com>
2023-12-27 00:16:11 +0200

    added header

--- app/js/main.js
+++ app/js/main.js
@@ -23,21 +23,93 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //swiper
 
-    const swiper = new Swiper('.swiper', {
-    
-    slidesPerView: 1,
-    autoplay: {
-      delay: 2000,
-    },
-      loop: true,
-    });
+    // const swiper = new Swiper('.swiper', {
+    // slidesPerView: 1,
+    // autoplay: {
+    //   delay: 6000,
+    // },
+    //   loop: true,
+    // });
     
 
-    $(function () {
-      if (window.location.pathname === '/works.html') {
-        var mixer = mixitup('.works__content');
+
+  
+
+  const resizableSwiper = (breakpoint, swiperClass, swiperSettings, callback) => {
+    let swiper;
+
+    breakpoint = window.matchMedia(breakpoint);
+
+    const enableSwiper = function(className, settings) {
+      swiper = new Swiper(className, settings);
+
+      if (callback) {
+        callback(swiper);
       }
-    });
+    }
+
+    const checker = function() {
+      if (breakpoint.matches) {
+        return enableSwiper(swiperClass, swiperSettings);
+      } else {
+        if (swiper !== undefined) swiper.destroy(true, true);
+        return;
+      }
+    };
+
+    breakpoint.addEventListener('change', checker);
+    checker();
+  }
+
+  const someFunc = (instance) => {
+    if (instance) {
+      instance.on('slideChange', function (e) {
+        console.log('*** mySwiper.activeIndex', instance.activeIndex);
+      });
+    }
+  };
+
+  resizableSwiper(
+    '(max-width: 1024px)',
+    '.slider-1',
+    {
+      loop: true,
+      spaceBetween: 32,
+      slidesPerView: 1,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    },
+    someFunc
+  );
+
+  resizableSwiper(
+    '(max-width: 1024px)',
+    '.slider-2',
+    {
+      loop: true,
+      slidesPerView: "auto",
+      spaceBetween: 30,
+      freeMode: true,
+      breakpoints: {
+        480: {
+          
+        },
+        1024: {
+          
+          spaceBetween: 20,
+        },
+        pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+        
+      }
+    }
+  );
+
+
 
 });
 
