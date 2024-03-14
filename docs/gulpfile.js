7b6a1a6ca124b30b2115f78d7566fd8f2426ffa7
oilprint <irynakozlova.k@gmail.com>
2024-03-14 16:47:04 +0200

    finished index

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -38,7 +38,7 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     // 'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/mixitup/dist/mixitup.js',
+    // 'node_modules/mixitup/dist/mixitup.js',
     'node_modules/swiper/swiper-bundle.js',
     'app/js/modal.js',  
     'app/js/main.js'  
